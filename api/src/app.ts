eae5c2609746f1ec26459c6b3251095f3e3c6e09
VishwaLakpriya <vishwalakpriya99@gmail.com>
2025-10-14 21:45:35 +0530

    changed localhost

--- api/src/app.ts
+++ api/src/app.ts
@@ -14,7 +14,7 @@ app.use(express.json());
 
 // allow frontend origin to send cookies
 app.use(cors({
-  origin: ["http://localhost:5174"],
+  origin: ["http://localhost:5173"],
   credentials: true
 }));
 
